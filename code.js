a2857093eee088a68ec370059d487f76731ce041
Oleksii Kovtun <746jeb@gmail.com>
2024-04-15 14:13:06 +0300

    homework 1 - draft 1

---
+++ code.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// numbers variables
+let num1 = 10;
+let num2 = 20;
+
+// operations with numbers
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+let exponentiation = num1**2;
+
+// outputting the result
+console.log("Addition: " + addition);
+console.log("Subtraction: " + subtraction);
+console.log("Multiplication: " + multiplication);
+console.log("Division: " + division);
+console.log("Exponentiation: " + exponentiation);
